ffca00d551faaa817e61a24e7531001123fd0986
frobee <frobi.webdev@outlook.com>
2023-05-16 17:48:58 +0930

    extract-the-home-code-and-call-during-initialization

--- src/initialize-page.js
+++ src/initialize-page.js
@@ -1,3 +1,5 @@
+import loadHome from './home';
+
 const content = document.getElementById('content');
 
 function createHeader() {
@@ -19,26 +21,14 @@ function createHeader() {
 function createMain() {
   const main = document.createElement('div');
   main.setAttribute('class', 'main');
-  const option = document.createElement('div');
-  option.setAttribute('class', 'option');
 
-  const home = document.createElement('div');
-  home.setAttribute('class', 'home');
-
-  const homePhotoContainer = document.createElement('div');
-  homePhotoContainer.setAttribute('class', 'home-photo-container');
-  const homePhoto = document.createElement('div');
-  homePhoto.setAttribute('class', 'home-photo');
-  const homeText = document.createElement('div');
-  homeText.setAttribute('class', 'home-text');
-  homeText.innerHTML = 'Nonna emigrated from Italy years ago, and she brought her homestyle cooking with her.<br><br> Everything in our restaurant is handmade, from the pasta to the sauces...<br><br> We use only the freshest ingredients, and we cook everything to order.';
+  const selectedTile = document.createElement('div');
+  selectedTile.setAttribute('class', 'selected-tile');
 
   content.appendChild(main);
-  main.appendChild(option);
-  option.appendChild(home);
-  home.appendChild(homePhotoContainer);
-  homePhotoContainer.appendChild(homePhoto);
-  home.appendChild(homeText);
+  main.appendChild(selectedTile);
+
+  return selectedTile;
 }
 
 function createFooter() {
@@ -49,10 +39,10 @@ function createFooter() {
   content.appendChild(footer);
 }
 
-function initializePage() {
+export default function initializePage() {
   createHeader();
-  createMain();
+  loadHome(createMain());
   createFooter();
 }
 
-export default initializePage();
+// export default initializePage();
